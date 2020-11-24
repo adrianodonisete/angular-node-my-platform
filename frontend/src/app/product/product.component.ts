9a5c8d1611da16cd659e6babad1bc81b3ef07d0f
adrianodonisete <adrianodonisete@gmail.com>
2020-11-24 00:20:38 -0300

    observable without subscribe

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -1,26 +1,61 @@
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'app-product',
-    templateUrl: './product.component.html',
-    styleUrls: ['./product.component.css'],
+	selector: 'app-product',
+	templateUrl: './product.component.html',
+	styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-    products: Product[];
-    displayColumns: ['id', 'name', 'price', 'action'];
+	products: Product[];
+	displayColumns: ['id', 'name', 'price', 'action'];
 
-    constructor(private productService: ProductService) {}
+	prods$: Observable<Product[]>;
 
-    ngOnInit(): void {
-        // this.productService.read().subscribe(products => {
-        //     this.products = products;
-        // });
-        this.productService.readSystax().subscribe(products => {
-            this.products = products;
-        });
-    }
+	paidShow: boolean = true;
+	pendingShow: boolean = true;
+	canceledShow: boolean = true;
+
+	constructor(private productService: ProductService) {}
+
+	ngOnInit(): void {
+    this.prods$ = this.productService.read().pipe(
+			tap(console.log) 
+		);
+	}
+
+	ngReadSystax() {
+		// this.productService.readSystax().subscribe(products => {
+		//     this.products = products;
+		// });
+	}
+
+	allShow() {
+		this.paidShow = true;
+		this.pendingShow = true;
+		this.canceledShow = true;
+	}
+
+	onlyPaidShow() {
+		this.paidShow = true;
+		this.pendingShow = false;
+		this.canceledShow = false;
+	}
+
+	onlyPendingShow() {
+		this.paidShow = false;
+		this.pendingShow = true;
+		this.canceledShow = false;
+	}
+
+	onlyCanceledShow() {
+		this.paidShow = false;
+		this.pendingShow = false;
+		this.canceledShow = true;
+	}
 }
 
 /**
--- frontend/src/app/product/product.service.ts
+++ frontend/src/app/product/product.service.ts
@@ -1,63 +1,91 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Product } from './product.model';
 import { Observable, EMPTY } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, delay, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class ProductService {
-  baseUrl: string = 'http://localhost:3001/products';
-  appSystax: string = 'https://app.systax.com.br/platform/refresh-token';
+	baseUrl: string = 'http://localhost:3001/products';
+	appSystax: string = 'https://app.systax.com.br/platform/refresh-token';
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
+	constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl).pipe(
-      map((obj) => {
-        console.log(obj);
-        return obj;
-      }),
-      catchError((e) => this.errorHandler(e))
-    );
-  }
+	read(): Observable<Product[]> {
+		let count = 0;
+		return this.http
+			.get<Product[]>(this.baseUrl, {
+				headers: {
+					token: 'teste',
+				},
+				observe: 'body',
+				params: {
+					'filter[0]': ['email', 'systax.com'],
+				},
+			})
+			.pipe(
+				delay(3000),
+				map(obj => {
+					obj = obj.map(a => {
+						a.total = a.id * 2;
+						return a;
+					});
+					return obj;
+				}),
+				map(obj => obj.filter(a => a.id % 2 == 0)),
+        catchError(e => {
+          console.log(e)
+          return EMPTY;
+        })
+			);
+	}
 
-  readSystax(): Observable<Product[]> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        token:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOiJ7XCJ1c2VybmFtZVwiOlwiYWRyaWFuby5hcGljZm1cIixcImlkX2NsaWVudGVcIjpcIjExNjU3XCJ9IiwiaWF0IjoxNjA1MTUyMjE4LCJleHAiOjE2MDUxNTU4MTgsInVzdWFyaW8iOiIyMDAyNSIsImNsaWVudGVzIjpbIjExNjU3Il19.XQVD5Bn20YlZ-fr1hpE3f5HK2eLNcvRzuBiISxKx-W0',
-      }),
-    };
-    return this.http.get<Product[]>(this.appSystax, httpOptions).pipe(
-      map((obj) => {
-        console.log(obj);
-        return obj;
-      }),
-      catchError((e) => this.errorHandler(e))
-    );
-  }
+	readSystax(): Observable<Product[]> {
+		const a = new HttpParams();
+		a.append('filter', '1');
 
-  errorHandler(e: any): Observable<any> {
-    console.log(e);
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				token:
+					'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.' +
+					'eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOiJ7XCJ1c2VybmFt' +
+					'ZVwiOlwiYWRyaWFuby5hcGljZm1cIixcImlkX2NsaWVudGVcI' +
+					'jpcIjExNjU3XCJ9IiwiaWF0IjoxNjA1MTUyMjE4LCJleHAiOjE' +
+					'2MDUxNTU4MTgsInVzdWFyaW8iOiIyMDAyNSIsImNsaWVudGVzIjpbIjExNjU3Il19' +
+					'.XQVD5Bn20YlZ-fr1hpE3f5HK2eLNcvRzuBiISxKx-W0',
+			}),
+			params: a,
+		};
+		return this.http.get<Product[]>(this.appSystax, httpOptions).pipe(
+			map(obj => {
+				console.log(obj);
+				return obj;
+			}),
+			catchError(e => this.errorHandler(e))
+		);
+	}
 
-    this.showMessage('Ocorreu um erro!', true);
-    return EMPTY;
-  }
+	errorHandler(e: any): Observable<any> {
+		console.log(e);
 
-  showMessage(msg: string, isError: boolean = false): void {
-    console.log(msg, isError);
+		this.showMessage('Ocorreu um erro!', true);
+		return EMPTY;
+	}
 
-    // this.snackBar.open(msg, 'X', {
-    //   duration: 3000,
-    //   horizontalPosition: 'right',
-    //   verticalPosition: 'top',
-    //   panelClass: isError ? ['msg-error'] : ['msg-success'],
-    // });
-  }
+	showMessage(msg: string, isError: boolean = false): void {
+		console.log(msg, isError);
+
+		// this.snackBar.open(msg, 'X', {
+		//   duration: 3000,
+		//   horizontalPosition: 'right',
+		//   verticalPosition: 'top',
+		//   panelClass: isError ? ['msg-error'] : ['msg-success'],
+		// });
+	}
 }
 
 // map(obj => {
